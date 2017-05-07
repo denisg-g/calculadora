af66c4a8ee8b923d44de51d4b17ac42da2ccab98
Jarvisnic <aaronmerk2loke9624@gmail.com>
2017-05-07 09:40:46 -0600

    operations & rails upgrade

--- Gemfile
+++ Gemfile
@@ -2,9 +2,11 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.7.1'
+#gem 'rails', '4.2.7.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
+
+gem 'rails', '~> 5.0.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -44,4 +46,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,26 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+
+def params_vars
+  @vars1 = :na,:cpia, :fb, :nb, :cpib, :fa
+  @vars2 = :cpia, :fb, :nb, :cpib, :fa
+  @vars3 = :fb, :nb, :cpib, :fa
+  @vars4 = :nb, :cpib, :fa
+  @vars5 = :cpib, :fa
+  @vars6 = :fa
+end
+
   def calcular
-  	if npor
-  		redirect_to root_path, alert: 'npor'
+  	 if (:npor.present? && :cpia.blank? && :fb.blank? && :nb.blank? && :cpib.blank? && :fa.blank?)
+       #operacion!!
+  		redirect_to root_path, alert: 'Operación registrada '
+      #render :json
+
+    elsif !(:npor.blank? && :cpia.blank? && :fb.blank? && :nb.blank? && :cpib.blank? && :fa.blank?)
+      redirect_to root_path, alert: 'ingresa al menos un valor papu'
   	end
-  end
+end
 
 end
